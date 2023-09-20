bd4547fa55e0c7a1f383a08cdc7e4b9dcfcbd5d8
root <root@Vesco-pc>
2023-09-21 01:27:37 +0300

    initial

---
+++ pkg/topic/kafka.go
@@ -0,0 +1,15 @@
+package topic
+
+const (
+	EventReportData = "logger.report.event"
+	UserReportData  = "logger.report.user"
+)
+
+const (
+	ETL = "bi.etl"
+)
+
+const (
+	LoggerSaverQueue = "logger-saver"
+	EtlQueue         = "bi-etl"
+)
